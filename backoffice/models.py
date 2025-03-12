abc71219971acdba6023912d9853a1115ca24735
David Pettersson <david@shebang.nu>
2025-03-12 17:39:42 -0400

    Reformatting

--- backoffice/models.py
+++ backoffice/models.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Member(models.Model):
     full_name = models.CharField(max_length=128)
