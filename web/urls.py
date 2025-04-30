fc2c418673579998292484e9f99e071d4187cd7d
David Pettersson <david@shebang.nu>
2025-04-29 21:39:08 -0400

    Basic cancellation flow for members

--- web/urls.py
+++ web/urls.py
@@ -6,6 +6,7 @@ from web.views.events import redirect_to_event_list, event_detail, event_list
 from web.views.login import LoginFormView
 from web.views.registrations import registration_create, registration_detail, registration_submitted, registration_list, \
     get_speed_ranges
+from web.views.profile import profile, registration_withdraw
 
 urlpatterns = [
     path("login/", LoginFormView.as_view(), name="login_form"),
@@ -15,9 +16,11 @@ urlpatterns = [
     path('events/<int:event_id>/registration', registration_create, name='registration_create'),
     path('events/<int:event_id>', event_detail, name='event_detail'),
     path('events', event_list, name='event_list'),
+    path('registrations/<int:registration_id>/withdraw', registration_withdraw, name='registration_withdraw'),
     path('registrations/<int:registration_id>', registration_detail, name='registration_detail'),
     path('registrations/submitted', registration_submitted, name='registration_submitted'),
     path('registrations', registration_list, name='registration_list'),
     path('rides/<int:ride_id>/speed-ranges', get_speed_ranges, name='get_speed_ranges'),
+    path('profile', profile, name='profile'),
     path('', redirect_to_event_list, name='index'),
 ]
