c3fd268c4ffeb3eea7a3d0174833577588f7b54e
David Pettersson <david@shebang.nu>
2025-05-03 20:05:05 -0400

    Added event rider page

--- web/views/events.py
+++ web/views/events.py
@@ -1,11 +1,12 @@
 from itertools import groupby
-from pprint import pprint
 
-from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
+from django.utils import timezone
 
-from backoffice.models import Event
+from backoffice.models import Event, Registration
+
 
 def redirect_to_event_list(request: HttpRequest) -> HttpResponseRedirect:
     return redirect('event_list')
@@ -41,3 +42,52 @@ def event_list(request: HttpRequest) -> HttpResponse:
     }
 
     return render(request, 'web/events/list.html', context)
+
+
+@login_required
+def event_riders(request: HttpRequest, event_id: int) -> HttpResponse:
+    # Check if user is registered as a ride leader for this event
+    registration = get_object_or_404(
+        Registration,
+        event_id=event_id,
+        user=request.user,
+        state=Registration.STATE_CONFIRMED,
+        ride_leader_preference=Registration.RIDE_LEADER_YES
+    )
+
+    # Ensure the event requires ride leaders
+    event = registration.event
+    if not event.ride_leaders_wanted:
+        return redirect('profile')
+
+    # Get all confirmed registrations for this event
+    registrations = Registration.objects.filter(
+        event_id=event_id,
+        state=Registration.STATE_CONFIRMED
+    ).select_related('ride', 'speed_range_preference', 'user')
+
+    # Group registrations by ride and speed preference
+    rides_dict = {}
+    for reg in registrations:
+        if reg.ride:
+            if reg.ride.id not in rides_dict:
+                rides_dict[reg.ride.id] = {
+                    'ride': reg.ride,
+                    'speed_ranges': {}
+                }
+
+            speed_range_id = reg.speed_range_preference.id if reg.speed_range_preference else 'none'
+            if speed_range_id not in rides_dict[reg.ride.id]['speed_ranges']:
+                rides_dict[reg.ride.id]['speed_ranges'][speed_range_id] = {
+                    'speed_range': reg.speed_range_preference,
+                    'riders': []
+                }
+
+            rides_dict[reg.ride.id]['speed_ranges'][speed_range_id]['riders'].append(reg)
+
+    context = {
+        'event': event,
+        'rides': rides_dict.values()
+    }
+
+    return render(request, 'web/events/riders.html', context=context)
