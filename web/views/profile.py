667234fcc9b4bb05ddf8351545e1939060e32b2f
David Pettersson <david@shebang.nu>
2025-05-03 20:24:22 -0400

    Do not show historical events

--- web/views/profile.py
+++ web/views/profile.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from datetime import timedelta
 
 from backoffice.models import Registration
 
@@ -10,7 +12,12 @@ def profile(request: HttpRequest) -> HttpResponse:
     """
     User profile view that shows all user registrations.
     """
-    user_registrations = Registration.objects.filter(user=request.user).order_by('event__starts_at')
+    # Filter out events that started more than 24 hours ago
+    twenty_four_hours_ago = timezone.now() - timedelta(hours=24)
+    user_registrations = Registration.objects.filter(
+        user=request.user,
+        event__starts_at__gte=twenty_four_hours_ago
+    ).order_by('event__starts_at')
     
     # Check if user is a ride leader for any event
     is_ride_leader = user_registrations.filter(
