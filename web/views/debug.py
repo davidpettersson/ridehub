b7e206a711ec601d625d350b8c73e6319ccbe549
David Pettersson <david@shebang.nu>
2025-05-09 00:12:08 -0400

    Added trigger error view and improved 500

--- web/views/debug.py
+++ web/views/debug.py
@@ -3,6 +3,10 @@ from django.http import HttpRequest, HttpResponse
 from backoffice.tasks import perform_task_in_job
 
 
+def trigger_error(request: HttpRequest) -> HttpResponse:
+    return 1 / 0
+
+
 def trigger_task(request: HttpRequest) -> HttpResponse:
     print('TRIGGER_TASK')
     perform_task_in_job.delay()
