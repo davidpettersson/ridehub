7b64cc15d647fb19048cef1f6caa885a90cf901f
David Pettersson <david@shebang.nu>
2025-06-10 20:04:29 -0400

    Changed list view

--- backoffice/admin.py
+++ backoffice/admin.py
@@ -97,9 +97,10 @@ class MemberAdmin(admin.ModelAdmin):
 
 
 class AnnouncementAdmin(admin.ModelAdmin):
-    list_display = ('begin_at', 'end_at', 'title', 'type')
+    list_display = ('title', 'type', 'begin_at', 'end_at',)
     search_fields = ('title', 'text',)
     list_filter = ('type',)
+    ordering = ('-end_at',)
 
 
 
