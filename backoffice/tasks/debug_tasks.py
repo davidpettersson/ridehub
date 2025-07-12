ba165c2ad4329328eb9934f37f882653232200ea
David Pettersson <david@shebang.nu>
2025-07-11 21:05:54 -0400

    Fixed broken formatting

--- backoffice/tasks/debug_tasks.py
+++ backoffice/tasks/debug_tasks.py
@@ -1,12 +1,9 @@
-from celery import shared_task
-
-
-@shared_task
-def perform_task_in_job():
 import logging
+
 from celery import shared_task
 
 logger = logging.getLogger(__name__)
+
 
 @shared_task
 def perform_task_in_job():
