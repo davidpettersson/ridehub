74bcb9791efd603fb9297569316ee28e35f9e13d
David Pettersson <david@shebang.nu>
2025-11-02 22:00:03 -0500

    Improved profile page with past registrations and basic stats

--- web/views/profile.py
+++ web/views/profile.py
@@ -8,10 +8,15 @@ from backoffice.services.registration_service import RegistrationService
 
 @login_required
 def profile(request: HttpRequest) -> HttpResponse:
-    registrations = RegistrationService().fetch_current_registrations(request.user)
+    registration_service = RegistrationService()
+    registrations = registration_service.fetch_current_registrations(request.user)
+    past_registrations = registration_service.fetch_past_registrations(request.user)
+    statistics = registration_service.fetch_user_statistics(request.user)
 
     context = {
         'registrations': registrations,
+        'past_registrations': past_registrations,
+        'statistics': statistics,
     }
 
     return render(request, 'web/profile/profile.html', context=context)
@@ -20,9 +25,9 @@ def profile(request: HttpRequest) -> HttpResponse:
 @login_required
 def registration_withdraw(request: HttpRequest, registration_id: int) -> HttpResponseRedirect:
     registration = get_object_or_404(Registration, id=registration_id, user=request.user)
-    
+
     if registration.state == 'confirmed' and request.method == 'POST':
         registration.withdraw()
         registration.save()
-        
+
     return redirect('profile')
