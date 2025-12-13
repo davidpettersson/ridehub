ad3a314cba5578741bee702e38ad5f9fdc529a8a
David Pettersson <david@shebang.nu>
2025-12-13 14:13:38 -0500

    Site name fix

--- backoffice/apps.py
+++ backoffice/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.contrib import admin
 
 
 class BackofficeConfig(AppConfig):
@@ -7,3 +8,6 @@ class BackofficeConfig(AppConfig):
 
     def ready(self):
         import backoffice.signals
+        admin.site.site_header = "RideHub"
+        admin.site.site_title = "RideHub"
+        admin.site.index_title = "Site Administration"
