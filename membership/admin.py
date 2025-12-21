06c8f041762d51ac7f6fe508726675b00dfd2ee6
David Pettersson <david@shebang.nu>
2025-12-20 19:14:36 -0500

    Import command

--- membership/admin.py
+++ membership/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from membership.models import Member, Registration
 
-admin.site.register(Member)
-admin.site.register(Registration)
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'cohort',)
+    list_filter = ('cohort',)
+    list_display_links = ('first_name',)
+
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('identity', 'first_name', 'last_name', 'registered_at', )
+    list_display_links = ('identity',)
+    date_hierarchy = 'registered_at'
+    ordering = ('-registered_at',)
+
+admin.site.register(Member, MemberAdmin)
+admin.site.register(Registration, RegistrationAdmin)
--- membership/models.py
+++ membership/models.py
@@ -1,53 +1,5 @@
 from django.db import models
 
-class Registration(models.Model):
-    registered_at = models.DateTimeField(
-        help_text='Date of registration in CCN Bikes',
-    )
-
-    identity = models.IntegerField(
-        help_text='Registration identity provided by CCN Bikes',
-    )
-
-    first_name = models.CharField(
-        max_length=64,
-    )
-
-    last_name = models.CharField(
-        max_length=64,
-    )
-
-    sex = models.CharField(
-        max_length=16,
-    )
-
-    date_of_birth = models.DateField(
-    )
-
-    category = models.CharField(
-        max_length=64,
-    )
-
-    country = models.CharField(
-        max_length=32,
-    )
-
-    postal_code = models.CharField(
-        max_length=16,
-    )
-
-    email = models.EmailField(
-        max_length=64,
-    )
-
-    phone = models.CharField(
-        max_length=32,
-    )
-
-    member_for = models.CharField(
-        max_length=16,
-        help_text='Self-reported membership length'
-    )
 
 class Member(models.Model):
     first_name = models.CharField(
@@ -87,4 +39,76 @@ class Member(models.Model):
 
     cohort = models.DateField(
         help_text='Identified year+month cohort. Day always 1.',
+    )
+
+
+class Registration(models.Model):
+    registered_at = models.DateTimeField(
+        help_text='Date of registration in CCN Bikes',
+    )
+
+    identity = models.IntegerField(
+        help_text='Registration identity provided by CCN Bikes',
+    )
+
+    first_name = models.CharField(
+        max_length=64,
+    )
+
+    last_name = models.CharField(
+        max_length=64,
+    )
+
+    sex = models.CharField(
+        max_length=16,
+    )
+
+    date_of_birth = models.DateField(
+    )
+
+    category = models.CharField(
+        max_length=64,
+    )
+
+    city = models.CharField(
+        max_length=64,
+    )
+
+    country = models.CharField(
+        max_length=32,
+    )
+
+    postal_code = models.CharField(
+        max_length=16,
+    )
+
+    email = models.EmailField(
+        max_length=64,
+    )
+
+    phone = models.CharField(
+        max_length=32,
+    )
+
+    duration = models.CharField(
+        max_length=32,
+        help_text='Self-reported membership duration'
+    )
+
+    member = models.ForeignKey(
+        to=Member,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        help_text='Linked to member record'
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        null=True,
     )
