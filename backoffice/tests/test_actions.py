2d80df1e37351078d6918cdfb4386d93020dc4ed
David Pettersson <david@shebang.nu>
2026-01-22 21:15:46 -0500

    Optimized imports

--- backoffice/tests/test_actions.py
+++ backoffice/tests/test_actions.py
@@ -1,8 +1,7 @@
 from datetime import timedelta
 
-from django.contrib.admin.sites import AdminSite
 from django.contrib.auth import get_user_model
-from django.test import TestCase, RequestFactory
+from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
